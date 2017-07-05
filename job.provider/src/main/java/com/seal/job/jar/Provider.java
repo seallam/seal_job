1b333af0ec6235dfcff2f72dcef7139cdff919fd
seal <seal@lianj.com>
2017-07-05 22:26:09 +0800

    新增spring和dubbo,增加配置文件,增加gitignore

--- job.provider/src/main/java/com/seal/job/jar/Provider.java
+++ job.provider/src/main/java/com/seal/job/jar/Provider.java
@@ -10,15 +10,42 @@ import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
 import com.seal.job.executor.SpiderSimpleJob;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Hasee on 2017/6/26.
  */
 public class Provider {
-    private static Logger logger = LoggerFactory.getLogger(Provider.class);
+    private static final Logger logger = LoggerFactory.getLogger(Provider.class);
+    private static volatile boolean running = true;
 
-    public static void main(String[] args) {
-        new JobScheduler(getRegCenter(), createJobConfiguration()).init();
+    public static void main(final String[] args) {
+        try {
+            String profile = "development";
+            if (args != null && args.length > 0) {
+                profile = args[0];
+            }
+            System.setProperty("spring.profiles.active", profile);
+            new ClassPathXmlApplicationContext("conf/seal-job-context.xml");
+            new JobScheduler(getRegCenter(), createJobConfiguration()).init();
+            logger.info(new SimpleDateFormat("[yyyy-MM-dd HH:mm:ss]").format(new Date()) + " service server started!");
+        } catch (RuntimeException e) {
+            logger.error(e.getMessage(), e);
+            e.printStackTrace();
+            System.exit(1);
+        }
+//        synchronized (Main.class) {
+//            while (running) {
+//                try {
+//                    Provider.class.wait();
+//                } catch (Throwable e) {
+//                }
+//            }
+//        }
+        com.alibaba.dubbo.container.Main.main(args);
     }
 
     private static CoordinatorRegistryCenter getRegCenter() {
